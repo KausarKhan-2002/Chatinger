aaad58bc3079fa521bea5a9086b8e09feb08d0d2
Kausar Khan <kausarrkhan83@gmail.com>
2025-05-22 23:56:11 +0530

    allowed domain origin

--- Server/app.js
+++ Server/app.js
@@ -7,13 +7,29 @@ const { messageRoute } = require("./src/routes/messageRoute");
 const { server, app } = require("./src/socketIO/server");
 
 app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chatinger-alpha.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 const PORT = 4000;
